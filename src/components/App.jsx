9b7a6d131b4fea456a83aa46d1d1a84d9ea843f9
Takashi Nishikawa <program.takashi@gmail.com>
2019-09-30 17:21:23 +0900

    [add] add sumthing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import Redux from './Redux.js';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         hello world
+        <Redux />
       </div>
     );
-  }  
+  }
 }
 
 export default App;
--- src/components/redux.js
+++ src/components/redux.js
@@ -1,3 +1,11 @@
-(()=>{
+import React from 'react';
 
-})();
+function Redux (){
+  return(
+    <div>
+      <input type='submit' value='発火'></input>
+    </div>
+  );
+}
+
+export default Redux;
