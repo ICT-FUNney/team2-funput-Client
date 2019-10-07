a40b0bbd4d1dae343090966e153ebd0f097d1dc5
KeitaOsaki <keita.base.05@gmail.com>
2019-10-07 15:51:37 +0900

    [ファイルの追加]Uploadファイルを追加しました

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Upload from './Upload';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         hello world
+        <Upload/>
       </div>
     );
   }  
