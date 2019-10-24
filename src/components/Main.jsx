50fd23bd2454e993208fd84983fcb004480c8bca
Hanyo Shunta <b1018162.fun@gmail.com>
2019-10-24 22:21:57 +0900

    [fix]:reacthooksで動くように修正

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,56 +1,53 @@
-import React from "react"
-import DropZone from "react-dropzone"
+import React, {useState,useCallback} from 'react'
+import { useDropzone } from 'react-dropzone'
 
 
 
-class Main extends React.Component {
+function Main()  {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      imageData : [],
-      value1 :'hogehoge',
-      value2 : '',
-    }
-    this.handleClick = this.handleClick.bind(this);
-  }
-    onDrop = (acceptedFiles) => {//画像ドロップ
+const [value1, setValue1] = useState('hogehoge');
+const [value2, setValue2] = useState("");
+
+
+   
+
+
+
+const onDrop = useCallback(acceptedFiles => {//画像ドロップ
    
       console.log(acceptedFiles);
-      this.setState({
+      setValue2({
         value2: acceptedFiles
       });
-    }
+    }, []);
+
+const { isDragActive,getRootProps,getInputProps,acceptedFiles} = useDropzone ({
+  onDrop
+});
     
-
+/*
     handleClick(){
-      alert(JSON.stringify(this.state.value2[0].lastModified));
+      alert(JSON.stringify(value2[0].lastModified));
     }
+    */
   
-    render() {
 
-      
       return (
         <div className="text-center mt-5">
-          <DropZone onDrop={this.onDrop}>
-            {({getRootProps, getInputProps, isDragActive}) => (
-              <div {...getRootProps()}>
+          
+          <div {...getRootProps()}>
+              
                 <input {...getInputProps()} />
-                {/* Click me to upload a file! */}
+                {!isDragActive && 'Click here or drop a file to upload!'}
+                
+              </div>
 
                 {isDragActive ? "Drop it like it's hot!" :'Click me or drag a file to upload!'}
     
-              </div>
-            )}
-
-          </DropZone>
-          <button className ="imgButton" onClick ={this.handleClick}>投稿画像</button>
-          
-          
-         
-        </div>
+                <button className ="imgButton" onClick ={() => setValue2()}>投稿画像
+               </button>
+         </div>
       );
-    }
-  }
+    };
 
 export default Main
