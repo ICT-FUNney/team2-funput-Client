8c5c1598c4e7fa934ff8c60df743068728e1ff7d
KeitaOsaki <keita.base.05@gmail.com>
2019-11-11 16:37:19 +0900

    [追加]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Upload from './Upload';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        hello world
+        <Upload/>
       </div>
     );
   }  
---
+++ src/components/resizeImage.js
@@ -0,0 +1,6 @@
+import loadImage from 'blueimp-load-image';
+
+
+const toBlob = (base64,reject)=>{
+    
+}
