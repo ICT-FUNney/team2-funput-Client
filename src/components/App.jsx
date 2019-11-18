25e938e42fe84fbfabdf480d9e98d8f74b9540ef
Takashi Nishikawa <program.takashi@gmail.com>
2019-11-18 15:46:08 +0900

    [add] 投稿物を追加するためのFormコンポーネントの作成

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,28 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Header from './Areas/Header';
-import {useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import '../styles/App.css';
 import Main from './Pages/Main';
 import Post from './Pages/Post';
-import {getExampleRequest} from '../actions/Example/ActionCreator';
-import {getUsersRequest} from '../actions/Users/ActionCreator';
-import {getWorksRequest} from '../actions/Works/ActionCreator';
-import {postWorksRequest} from '../actions/Works/ActionCreator';
-import {postWorksDataRequest} from '../actions/Works/ActionCreator';
+import { getExampleRequest } from '../actions/Example/ActionCreator';
+import { getUsersRequest } from '../actions/Users/ActionCreator';
+import { getWorksRequest } from '../actions/Works/ActionCreator';
+import FormWorkData from '../components/Elements/FormWorkData'
 
 
 const App = () => {
   const worksData = ["workPostTitle.values", "file_urls", "workPostComment.values"];
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getExampleRequest());
     dispatch(getUsersRequest());
     dispatch(getWorksRequest());
-    //dispatch(postWorksDataRequest(worksData));
   });
 
   return (
     <div id="container">
       <Header />
       <Main />
-      <Post />
     </div>
   )
 }
--- src/components/Elements/FormWorkData.jsx
+++ src/components/Elements/FormWorkData.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import {postWorksDataRequest} from '../../actions/Works/ActionCreator';
 import {useDispatch } from 'react-redux';
 import '../../styles/FormWorkData.css'
@@ -6,36 +6,40 @@ import '../../styles/FormWorkData.css'
 
 const FormWorkData = () => {
   const dispatch = useDispatch();
+  const [workPostTitle, setworkPostTitle] = useState('');
+  const onWorkPostTitleChange = (e) =>{
+    setworkPostTitle(e.currentTarget.value);
+  }
+  const [workPostComment, setworkPostComment] = useState('');
+  const onWorkPostComment = (e) =>{
+    setworkPostComment(e.currentTarget.value);
+  }
   const handleClickSubmitt = function(e){
     e.preventDefault();
-    const workPostTitle = document.getElementById("workPostTitle");
-    const workPostComment = document.getElementById("workPostComment");
-    const worksData = [workPostTitle.value, "file_url", workPostComment.value];
-    console.log(worksData);
-    console.log(workPostTitle.value);
-    console.log(workPostComment.value);
+    if(workPostTitle==''|| workPostComment==''){
+      return;
+    }
+    const worksData = [workPostTitle, "file_url", workPostComment];
     dispatch(postWorksDataRequest(worksData));
-    workPostTitle.value="";
-    workPostComment.value="";
+    setworkPostTitle("");
+    setworkPostComment("");
   }
 
   return (
 
     <div id="formWorkData">
       <div className="container">
-        <form>
           <div id="wpt">
             <div>
-              <input id="workPostTitle" type="text" placeholder='作品名'></input>
+              <input id="workPostTitle" type="text" value={workPostTitle} onChange={onWorkPostTitleChange} placeholder='作品名'></input>
             </div>
           </div>
           <div id="wpc">
             <div>
-              <textarea id = "workPostComment" placeholder='作品名に関するコメント'></textarea>
+              <textarea id = "workPostComment" value={workPostComment} onChange = {onWorkPostComment} placeholder='作品名に関するコメント'></textarea>
             </div>
           </div>
           <input id = 'submit' type="submit" value="投稿する" onClick={ handleClickSubmitt}/>
-        </form>
       </div>
     </div>
   )
