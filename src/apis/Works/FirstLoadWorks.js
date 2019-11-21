f262ee0c84ee8925058b6f8c5acf69f07e65f207
Takashi Nishikawa <program.takashi@gmail.com>
2019-11-21 09:57:53 +0900

    [refactor] 試作版からテスト用の記述のみを削除

--- src/apis/Works/FirstLoadWorks.js
+++
@@ -1,9 +0,0 @@
-import Axios from "../Axios";
-
-export async function firstLoadWorks(){
-  try{
-    return await Axios.get('/works');
-  }catch(e){
-    throw new Error(e);
-  }
-}
--- src/components/Areas/ViewWorks.jsx
+++ src/components/Areas/ViewWorks.jsx
@@ -3,16 +3,6 @@ import Work from '../Elements/Work';
 import { useDispatch, useSelector } from 'react-redux';
 import { getWorksRequest } from '../../actions/Works/ActionCreator';
 
-// async function getData(){
-//   const dispatch = useDispatch();
-//   await useEffect(() => {
-//     dispatch(getWorksRequest());
-//   }, [])
-//   const works = useSelector(state => state.works);
-//   console.log(works);
-//   return works;
-// }
-
 function useUserHook() {
 
   const dispatch = useDispatch();
@@ -21,68 +11,26 @@ function useUserHook() {
   }, [])
 
   const works = useSelector(state => state.works);
-  console.log(works);
   return works;
 }
 
-
 const ViewWorks = (props) => {
   const data = useUserHook();
-  console.log(data);
-  if(Object.keys(data.works).length){
-    console.log(data.works.b1018001);
-  }
-  Object.keys(data.works).length ? Object.keys(data.works.b1018001.uploads).forEach(function(key){
-    console.log(data.works.b1018001.uploads[key]);
-  }) : console.log("false")
 
+  return (
+    <div>
+      ViewWorks
+      <div>
+        {
+          Object.keys(data.works).length ? Object.keys(data.works.b1018001.uploads).map((key,index)=>{
+            console.log(data.works.b1018001.uploads[key]);
+            return <Work userName="hoge" description={data.works.b1018001.uploads[key].description} url={data.works.b1018001.uploads[key].file_url} key={index}/>
+          }) : <div>false</div>
+        }
 
-  const data1 = {
-    "work_title": "タイトル２",
-    "file_url": "",
-    "description": "コメントだよー"
-  };
-  const data2 = {
-    "work_title": "typescript clean architecture",
-    "file_url": "https://firebasestorage.googleapis.com/v0/b/takashi-s-works-uploads.appspot.com/o/images%2FThe%20Girl%20of%20the%20nightjpg.jpg?alt=media&amp;token=4655abd4-94bb-414e-9c01-3f8be485b283",
-    "description": "〜をレビューして欲しいですaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-  };
-  const worksDatas = [data1, data2]
-    return (
-        <div>
-          ViewWorks
-          <div>
-            {
-              worksDatas.map((element,index) => {
-                return <Work userName="hoge" description={element.description} url={element.file_url} key={index}/>
-              })
-            }
-          </div>
-          <div>
-            hi
-            {
-              Object.keys(data.works).length ? Object.keys(data.works.b1018001.uploads).map((key,index)=>{
-                console.log(data.works.b1018001.uploads[key]);
-                return <div key = {index}>
-                  <div key="work_title">{data.works.b1018001.uploads[key].work_title}</div>
-                  <div key ="file_url">{data.works.b1018001.uploads[key].file_url}</div>
-                  <div key="description">{data.works.b1018001.uploads[key].description}</div>
-                </div>
-              }) : <div>false</div>
-            }
-          </div>
-          <div>
-            hi2
-            {
-              Object.keys(data.works).length ? Object.keys(data.works.b1018001.uploads).map((key,index)=>{
-                console.log(data.works.b1018001.uploads[key]);
-                return <Work userName="hoge" description={data.works.b1018001.uploads[key].description} url={data.works.b1018001.uploads[key].file_url} key={index}/>
-              }) : <div>false</div>
-            }
-
-          </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default ViewWorks;
