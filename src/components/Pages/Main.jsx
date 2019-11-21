08d8ecf1a61dca79b22303e74b204dddfd83e91e
Takashi Nishikawa <program.takashi@gmail.com>
2019-11-22 00:25:35 +0900

    [refactor]テスト用の文を削除

--- src/components/Pages/Main.jsx
+++ src/components/Pages/Main.jsx
@@ -3,14 +3,13 @@ import {Route} from 'react-router-dom';
 import Home from './Home';
 import Like from './Like';
 import Post from './Post';
-import ViewWorks from '../Areas/ViewWorks'
+
 const Main = () => {
   return (
     <div id='main'>{/*各urlにて表示させるcomponentを設定する*/ }
       <Route exact path='/home' component={Home} />
       <Route path='/like' component={Like} />
       <Route path='/post' component={Post} />
-      <ViewWorks />
     </div>
   )
 }
