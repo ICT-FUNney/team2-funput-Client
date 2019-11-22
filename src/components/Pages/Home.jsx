3db0cc934b3a33aecdc0c260c9ae97a404a383bf
ShotaroOkada <b1016117@gmail.com>
2019-11-22 21:11:54 +0900

    [fix]みんなの作品欄における並び順修正

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../styles/Pages/Home.css';
 import Profile from '../Areas/Profile';
+import ViewWorks from '../Areas/ViewWorks';
 
 const Home = () => {
     return (
@@ -9,7 +10,7 @@ const Home = () => {
                 <div id='profileArea'>
                     <Profile />
                 </div>
-                <div id='workArea'><p>みんなの作品</p></div>
+                <div id='workArea'><ViewWorks /></div>
             </div>
         </div>
     )
