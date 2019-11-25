f765954d3654d8abbee432c8b084e42edbaecadf
Hanyo Shunta <b1018162.fun@gmail.com>
2019-11-25 17:07:46 +0900

    [fix]若干の修正

--- src/components/Areas/Profile.jsx
+++ src/components/Areas/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Icon from '../Elements/Icon';
+import '../../styles/Areas/Profile.css';
+import Button from '../Elements/Button';
 
 const Profile = () => {
     return (
         <div>
             <Icon />
+            <Button />
         </div>
     )
 }
