4d1a6058077ce7a3c822e0e4aef5f2a5856e52e8
ShotaroOkada <b1016117@gmail.com>
2019-12-02 15:22:44 +0900

    [fix]userInfoとProfileコンポーネントの修正

--- src/components/Areas/UserInfo.jsx
+++ src/components/Areas/UserInfo.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Icon from '../Elements/Icon';
+import '../../styles/Areas/UserInfo.css';
+import Button from '../Elements/Button';
 
 const UserInfo = () => {
     return (
         <div>
-            UserInfo
+            <Icon />
+            <Button />
+            <span className='userInfoTitle'>自己紹介</span>
+            <div className='userInfoTitle'>今までの作品</div>
         </div>
     )
 }
