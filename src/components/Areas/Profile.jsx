8910b514bd7074c5f6b7c0cb76c95a550c7f435e
KeitaOsaki <keita.base.05@gmail.com>
2019-12-02 17:41:41 +0900

    [change]profile.cssを変更

--- src/components/Areas/Profile.jsx
+++ src/components/Areas/Profile.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Icon from '../Elements/Icon';
+import '../../styles/Areas/Profile.css';
+import Button from '../Elements/Button';
 
 const Profile = () => {
     return (
         <div>
-            Profile
+            <Icon />
+            <Button />
+            <span className='profileTitle'>自己紹介</span>
+            <span className='profileTitle'>今までの作品</span>
         </div>
     )
 }
