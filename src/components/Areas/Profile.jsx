32cf5cc39795261a8fe6927265911754d1ea962e
KeitaOsaki <keita.base.05@gmail.com>
2019-12-08 00:34:07 +0900

    Profileエリアの文字の大きさの修正とworkコンポーネントの配置

--- src/components/Areas/Profile.jsx
+++ src/components/Areas/Profile.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import Icon from '../Elements/Icon';
 import '../../styles/Areas/Profile.css';
 import Button from '../Elements/Button';
+import Work from '../Elements/Work';
+import { useSelector } from 'react-redux';
 
-const Profile = () => {
+    const Profile = () => {
+    const works = useSelector(state => state.works);
     return (
         <div id = 'profileContainer'>
             <div id = 'IconArea'> <Icon /> </div>
            <div id = 'ButtonArea'> <Button/></div>
            <div id = 'profile'>
-            <p><span className='profileTitle'>自己紹介</span></p>
-            <span className='profileTitle'>今までの作品</span>
+               <span className='profileTitle'>自己紹介</span>
             </div>
+            <div id = 'Worked'>
+                <span className='profileTitle'>今までの作品</span>
+              </div>
+          <div id='MyWork'>
+            <div className='MyWorks-Works'>
+        {
+          Object.keys(works).length ? Object.keys(works.b1018001.uploads).map((key, index) => {
+            return <Work userName="hoge" index={index} description={works.b1018001.uploads[key].description} url={works.b1018001.uploads[key].file_url} key={index} />
+          }) :<div>false</div>
+    }
+      </div>
+      </div>
+                
+
         </div>
     )
 }
