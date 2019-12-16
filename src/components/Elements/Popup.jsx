5725bd3480dff938982dffcc338c77bfeffcf4f5
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2019-12-16 17:49:50 +0900

    [add]Popup

---
+++ src/components/Elements/Popup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/Elements/Popup.css'
+
+
+const Popup = () => {
+    return (
+        <div className="Popup">
+            <button>フォロー</button>
+        </div>
+    )
+}
+
+export default Popup;
