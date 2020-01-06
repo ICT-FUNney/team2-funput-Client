84e63211714cfb12f54210dff5d17ca74c7501e0
Hanyo Shunta <b1018162.fun@gmail.com>
2020-01-06 16:52:22 +0900

    [add]所持しているFunneyの表示

--- src/components/Areas/UserInfo.jsx
+++ src/components/Areas/UserInfo.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Icon from '../Elements/Icon';
 import '../../styles/Areas/UserInfo.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getFunnyRequest } from '../../actions/Funny/ActionCreator';
 
 
 const UserInfo = () => {
+    const dispatch = useDispatch();
+    const id =useSelector(state=> state.loginUser.id)
+    const token = useSelector(state => state.loginUser.token) 
+    console.log(id);
+    console.log(token);
+    const data = {
+        id,
+        password:''
+    }
+    useEffect(() => {
+        dispatch(getFunnyRequest(data,token))
+    })
     const Funny = useSelector(state => state.funny.funney)
     return (
         <div>
