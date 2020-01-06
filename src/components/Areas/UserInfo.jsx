2eed56ada3ad65f19a0228ec1070affd0ab799bc
Hanyo Shunta <b1018162.fun@gmail.com>
2020-01-06 15:57:21 +0900

    自分のFunnyを表示できるようデザインを修正

--- src/components/Areas/UserInfo.jsx
+++ src/components/Areas/UserInfo.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Icon from '../Elements/Icon';
 import '../../styles/Areas/UserInfo.css';
+import { useSelector } from 'react-redux';
 
 
 const UserInfo = () => {
+    const Funny = useSelector(state => state.users.balance)
     return (
         <div>
             <Icon />
             <p>フォロー</p>
             <p>フォロワー</p>
+            <hr/>
             <p>Funny残高</p>
-            <p className='funny'>Funny</p>
+            <p className='funny'>{Funny} Funny</p>
+            <hr/>
             <p>質問履歴</p>
             <p>コメント履歴</p>
         </div>
